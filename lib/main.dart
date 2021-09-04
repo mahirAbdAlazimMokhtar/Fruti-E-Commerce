a49f00fbaaff4216c97da9b152762cd360fb9bdd
mahir <mahr.bwb@gmail.com>
2021-09-04 10:22:34 +0200

    Finishing Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fruti_market_ecommerce/Features/splash/persentation/splash_view.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
   }
 }
