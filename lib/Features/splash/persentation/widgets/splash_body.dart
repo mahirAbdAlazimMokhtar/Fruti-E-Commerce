8a6407d192683ebd2e3b73151abe37cf6ce1ef38
mahir <mahr.bwb@gmail.com>
2021-09-04 10:22:59 +0200

    custom splash screen and add animation

---
+++ lib/Features/splash/persentation/widgets/splash_body.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({Key? key}) : super(key: key);
+
+  @override
+  _SplashViewBodyState createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  AnimationController? animationController;
+  Animation<double>? fadingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 600));
+    fadingAnimation =
+        Tween<double>(begin: .2, end: 1).animate(animationController!);
+    animationController?.repeat(reverse: true);
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    animationController?.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Spacer(),
+          FadeTransition(
+            opacity: fadingAnimation!,
+            child: Text(
+              'Fruit Market',
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 51,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff),
+              ),
+            ),
+          ),
+          Image.asset(
+            'assets/image/splash_view_image.png',
+          )
+        ],
+      ),
+    );
+  }
+}
