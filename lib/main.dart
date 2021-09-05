96c59e50f13beb2ef0d35d8879eeda1e36a20da1
mahir <mahr.bwb@gmail.com>
2021-09-05 11:54:22 +0200

    Finishing OnBoarding and Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Poppins'
+      ),
       debugShowCheckedModeBanner: false,
-      home: SplashView(),
+      home: SplashView(
+      ),
     );
   }
 }
